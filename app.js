69198b642e425713a79ed34ce57107064ec1d39e
Cristian Camacho <Cristian_Camacho@aol.com>
2022-07-04 17:39:15 -0400

    GOT able to make requests, having issues with using other modules.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express()
 const PORT = 3000
 
 app.use(express.json())
-
 app.use('/autoInfo', routes.autoInfo)
 
 app.get('/', (req, res) => {
--- controllers/autoInfo.controlls.js
+++ controllers/autoInfo.controlls.js
@@ -1,17 +1,17 @@
 const getYears = (req, res) => {
-    // let years = []
-    // fetch('https://www.fueleconomy.gov/ws/rest/vehicle/menu/year').then((res) => {
-    //     return res.text()
-    // }).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then((year) => {
-    //     console.log(year)
-    //     // for (let i = 0; i < year.getElementsByTagName('menuItem').length; i++) {
-    //     //     years.appendChild(year.getElementsByTagName('menuItem')[i].firstChild.innerHTML)
-    //     // }
-    // })
+    let years = []
+    got('https://www.fueleconomy.gov/ws/rest/vehicle/menu/year').then((res) => {
+        return res.text()
+    }).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then((year) => {
+        console.log(year)
+        // for (let i = 0; i < year.getElementsByTagName('menuItem').length; i++) {
+        //     years.appendChild(year.getElementsByTagName('menuItem')[i].firstChild.innerHTML)
+        // }
+    })
     res.send('test')
-    // res.json({
-    //     years: years
-    // })
+    res.json({
+        years: years
+    })
 }
 
 module.exports = {

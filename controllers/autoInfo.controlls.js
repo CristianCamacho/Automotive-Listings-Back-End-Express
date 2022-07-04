c21da5e7da9041b3dfefdb3f5214b5a08bf098f4
Cristian Camacho <Cristian_Camacho@aol.com>
2022-07-04 19:38:19 -0400

    Now properly imports node-fetch and xml2js to, able to fetch epa api and return all years.

--- controllers/autoInfo.controlls.js
+++ controllers/autoInfo.controlls.js
@@ -1,16 +1,25 @@
+const fetch = require('node-fetch')
+const parseString = require('xml2js').parseString
 const getYears = (req, res) => {
     let years = []
-    got('https://www.fueleconomy.gov/ws/rest/vehicle/menu/year').then((res) => {
+    fetch('https://www.fueleconomy.gov/ws/rest/vehicle/menu/year').then((res) => {
         return res.text()
-    }).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then((year) => {
-        console.log(year)
-        // for (let i = 0; i < year.getElementsByTagName('menuItem').length; i++) {
-        //     years.appendChild(year.getElementsByTagName('menuItem')[i].firstChild.innerHTML)
+    }).then((yearsXml) => {
+        // for (let i = 0; i < yearsXml.getElementsByTagName('menuItem').length; i++) {
+        //     years.appendChild(yearsXml.getElementsByTagName('menuItem')[i].firstChild.innerHTML)
         // }
-    })
-    res.send('test')
-    res.json({
-        years: years
+        parseString(yearsXml, function (error, result) {
+            if (error) {
+                console.log(error)
+                res.sendStatus(500)
+            } else {
+                res.json({
+                    years: result.menuItems.menuItem.map((element) => {
+                        return element.value[0]
+                    })
+                })
+            }
+        })
     })
 }
 
